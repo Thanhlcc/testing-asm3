4161eb555aefc29e74054a53069b0ce5c02399b6
thiendut0ngu <tranquangthien31@gmail.com>
2023-12-11 00:31:38 +0700

    thien full

--- test/searchcourse.spec.js
+++ test/searchcourse.spec.js
@@ -3,7 +3,7 @@ const { Builder, By, Key, until } = require('selenium-webdriver')
 const assert = require('assert')
 
 describe('search_course', function() {
-  // this.timeout(30000)
+  this.timeout(30000)
   let driver
   let vars
   beforeEach(async function() {
@@ -13,33 +13,18 @@ describe('search_course', function() {
   afterEach(async function() {
     await driver.quit();
   })
-  it('search_course', async function(done) {
+  it('search_course', async function() {
     await driver.get("https://school.moodledemo.net/my/courses.php")
     await driver.manage().window().setRect({ width: 1096, height: 700 })
     await driver.findElement(By.linkText("Log in")).click()
     await driver.findElement(By.id("username")).sendKeys("teacher")
     await driver.findElement(By.id("password")).click()
     await driver.findElement(By.id("password")).sendKeys("moodle")
-    // await new Promise((resolve) => setTimeout(resolve, 100));
     await driver.findElement(By.id("password")).sendKeys(Key.ENTER)
-    // await new Promise((resolve) => setTimeout(resolve, 100));
     await driver.findElement(By.name("search")).click()
-    // await new Promise((resolve) => setTimeout(resolve, 1000));
     await driver.findElement(By.name("search")).sendKeys("mount")
-    // {
-    //   const element = await driver.findElement(By.css("#displaydropdown > span"))
-    //   await driver.actions({ bridge: true }).moveToElement(element).perform()
-    // }
-    {
-      const element = await driver.findElement(By.CSS_SELECTOR, "body")
-      await driver.actions({ bridge: true }).moveToElement(element, 0, 0).perform()
-    }
+    await driver.sleep(5000)
     await driver.findElement(By.css("#sortingdropdown > span")).click()
     await driver.findElement(By.linkText("Sort by last accessed")).click()
-    {
-      const element = await driver.findElement(By.css(".card:nth-child(2) > a > .card-img"))
-      await driver.actions({ bridge: true }).moveToElement(element).perform()
-    }
-    done();
   })
 })
